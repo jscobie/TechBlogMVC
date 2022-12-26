85570cfb4024fd49f074b0065d04df2edfdb6549
Jacob Scobie <jscobie@focus-solutions.net>
2022-12-26 12:31:46 -0500

    api routes initial commit

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// Get all logged in user posts, which will display on /dashboard page
 router.get('/', withAuth, async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -41,3 +41,42 @@ router.get('/', withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+// Route for editing a users post from within dashboard
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+    const post = dbPostData.get({ plain: true });
+    res.render('editDeletePost', {
+      post,
+      logged_in: true,
+      username: req.session.username
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
